206952c77431c5eec2c84e278bafc58ce8598d51
Mich <125368412+michellycruz@users.noreply.github.com>
2023-08-28 23:31:02 -0300

    estruturas condicionais em java

---
+++ 06_logicos/Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {;
+    int a, b;
+
+    a = 4;
+    b = 8;
+
+    System.out.println(a > b);
+    System.out.println(a > b || a == 4);
+    System.out.println(a > b && a == 4);
+  }
+}
