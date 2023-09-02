1b68b8a466a4aef1049816dd0406ce684d9ffa8c
Mich <125368412+michellycruz@users.noreply.github.com>
2023-09-02 09:30:48 -0300

    testes com java

---
+++ 16_metodosjava/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static int somar(int a, int b){
+    return(a + b);
+  }
+  
+  public static void main(String[] args) {
+    int total = Main.somar(10, 4);
+    System.out.println(total);
+  }
+}
---
+++ 17_rankfilmes/Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    for(int filme = 0 ; filme <= 5 ; filme++){
+      if(filme == 1){
+        System.out.println(filme + "º - O rei do show");
+      } else if (filme == 2){
+        System.out.println(filme + "º - Ratatouille");
+      } else if (filme == 3){
+        System.out.println(filme + "º - A era do gelo");
+      } else if (filme == 4){
+        System.out.println(filme + "º - Todo mundo em pânico(todos)");
+      } else if (filme == 5) {
+        System.out.println(filme + "º - Rua do medo(todos)");
+      }
+    }
+  }
+}
