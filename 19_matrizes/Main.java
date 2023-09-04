9654c82ae2575b80f42bc923c5cfb58b4d23be2a
Mich <125368412+michellycruz@users.noreply.github.com>
2023-09-04 18:16:44 -0300

    matrizes

---
+++ 19_matrizes/Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int valor;
+    int[][] dados = new int [3][3];
+
+      for(int i = 0; i < 3 ; i++)
+        for(int j = 0; j < 3 ; j++)
+              dados[i][j] = j;
+    
+      for(int i = 0; i < 3 ; i++)
+        for(int j = 0; j < 3 ; j++)
+        System.out.println(dados[i][j]);
+  }
+}
