b52370424550406eced0649f93e3575487a2c6e9
Mich <125368412+michellycruz@users.noreply.github.com>
2023-09-04 17:55:36 -0300

    vetores java

---
+++ 18_vetores/Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    int valor;
+    int[] dados = new int [5];
+
+    dados[4] = 7;
+    dados[2] = 9;
+    dados[0] = 4;
+    dados[3] = 5;
+    dados[1] = 0;
+
+    System.out.println(dados[0]);
+  }
+}
