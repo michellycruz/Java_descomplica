495ac82b661bb38bcd3058872aef637052b88f5a
Mich <125368412+michellycruz@users.noreply.github.com>
2023-09-12 17:34:29 -0300

    classes abstratas

---
+++ 28_classesabstratas/Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    Conta cp = new Poupanca();
+    cp.setSaldo(5000);
+    cp.imprimeExtrato();
+  }
+}
---
+++ 28_classesabstratas/Poupanca.java
@@ -0,0 +1,5 @@
+public class Poupanca extends Conta{
+  public void imprimeExtrato(){
+    System.out.println("Saldo: " + this.getSaldo());
+  }
+}
