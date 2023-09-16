19b765e342e011fabb076d7b9c0acd8ee5d01e03
Mich <125368412+michellycruz@users.noreply.github.com>
2023-09-16 00:08:59 -0300

    collections

---
+++ 36_collection/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+class Main {
+  public static void main(String[] args) {
+    ArrayList<Integer> numeros = new ArrayList<>();
+    numeros.add(1);
+    numeros.add(3);
+    numeros.add(2);
+
+    System.out.println("ArrayList: " + numeros);
+
+
+    //criando uma instância de Iterator
+    Iterator<Integer> it = numeros.iterator();
+
+    int numero = it.next();
+    System.out.println("Elemento: " + numero);
+
+    while(it.hasNext()){
+      it.forEachRemaining((value) -> System.out.print(value + ", "));
+    }
+  }
+}
