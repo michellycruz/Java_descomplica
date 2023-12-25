41ec98d37186c4853dfb4971647fbd0d4f915228
Mich <125368412+michellycruz@users.noreply.github.com>
2023-12-25 16:15:11 -0300

    Add files via upload

---
+++ Modulo2/04_SerOuNaoSer/src/Principal.java
@@ -0,0 +1,18 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		int numero = 3;
+		
+		if(numero == 8) {
+			System.out.println("o valor do número é igual a 8: " + numero);
+		} else if (numero > 8){
+			System.out.println("o valor do número é maior que 8: " + numero);
+		} else {
+			System.out.println("o valor do número é menor que 8: " + numero);
+		}
+		
+	}
+
+}
