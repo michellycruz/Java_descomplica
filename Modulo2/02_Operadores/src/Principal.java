e5dd8ca60c068405684471aee01ba1a073857779
Mich <125368412+michellycruz@users.noreply.github.com>
2023-12-25 08:18:54 -0300

    Modulo 2 - "Programação extrema"

---
+++ Modulo2/02_Operadores/src/Principal.java
@@ -0,0 +1,25 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		byte x = 9;   //
+		short y = 2;
+		int z = 5;
+		long k = 6;
+		float u = 2;
+		double w = 3.0;
+		boolean t = true;
+		
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(k);
+		System.out.println(u);
+		System.out.println(w);
+		System.out.println(t);
+
+	}
+
+}
