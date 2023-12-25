e334fa8ad9c1bf1d5d6d78ada3585fd1ee346b62
Mich <125368412+michellycruz@users.noreply.github.com>
2023-12-25 17:26:22 -0300

    Add files via upload

---
+++ Modulo2/06_Switch/src/Principal.java
@@ -0,0 +1,24 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		int numero = 1;
+		
+		switch(numero){
+			case 1 :
+				System.out.println("Você digitou 1");
+				break;
+			case 2:
+				System.out.println("Você digitou 2");
+				break;
+			case 3:
+				System.out.println("Você digitou 3");
+				break;
+			default:
+				System.out.println("Opção inválida");
+		}
+
+	}
+
+}
