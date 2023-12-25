d94fe39ccab623425255663b8b91aaf85a6c3558
Mich <125368412+michellycruz@users.noreply.github.com>
2023-12-25 17:49:08 -0300

    Add files via upload

---
+++ Modulo2/07_While/src/Principal.java
@@ -0,0 +1,15 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		int numero = 1;
+		
+		while (numero <= 10) {
+			System.out.println(numero);
+			numero++;
+		}
+
+	}
+
+}
