6aa65d84add0c03abc1c31d5d9fdd54a639bddcf
Mich <125368412+michellycruz@users.noreply.github.com>
2024-01-21 20:11:56 -0300

    tratamento de erros no java

---
+++ Modulo2/29_Excecoes/src/pacotao/Principal.java
@@ -0,0 +1,16 @@
+package pacotao;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		try {
+			int resultado = 10 / 0;
+			System.out.println(resultado);
+		}catch(Exception e){
+			System.out.println("Erro: " + e.getMessage());
+		}
+
+	}
+
+}
