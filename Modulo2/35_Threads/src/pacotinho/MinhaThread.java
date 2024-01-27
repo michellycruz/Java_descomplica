5f950b11b65270acaf3a203d37275c633b9f4c5d
Mich <125368412+michellycruz@users.noreply.github.com>
2024-01-27 19:14:35 -0300

    threads java

---
+++ Modulo2/35_Threads/src/pacotinho/MinhaThread.java
@@ -0,0 +1,15 @@
+package pacotinho;
+
+public class MinhaThread extends Thread {
+	public void run() 
+	{
+		System.out.println("Thread Secundária");
+	}
+	public static void main(String[] args) 
+	{
+		MinhaThread minhaThread = new MinhaThread();
+		minhaThread.start();
+		System.out.println("Thread Principal");
+	}
+
+}
